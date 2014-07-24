9900d691a5bf9646eccd8d0910ccb89441ce50e9
Miro Cillik <miro@keboola.com>
2014-07-24 14:42:07 +0000

    fix: oauth controller

--- Controller/OauthController.php
+++ Controller/OauthController.php
@@ -18,6 +18,7 @@ use Keboola\Google\DriveBundle\Exception\ParameterMissingException;
 use Keboola\StorageApi\ClientException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Attribute\AttributeBag;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -68,13 +69,15 @@ class OauthController extends BaseController
 		return $this->container->get('google_rest_api');
 	}
 
-	public function externalAuthAction()
+	public function externalAuthAction(Request $request)
 	{
-		$request = $this->getRequest();
-
 		// check token - if expired redirect to error page
 		try {
-			$sapi = new StorageApi($request->query->get('token'), null, $this->componentName);
+			$sapi = new StorageApi([
+				'token' => $request->query->get('token'),
+				'url'   => null,
+				'userAgent' => $this->componentName
+			]);
 		} catch (ClientException $e) {
 
 			if ($e->getCode() == 401) {
@@ -96,9 +99,9 @@ class OauthController extends BaseController
 		return $this->render('KeboolaGoogleDriveBundle:Oauth:finish.html.twig');
 	}
 
-	public function oauthAction()
+	public function oauthAction(Request $request)
 	{
-		if (!$this->getRequest()->request->get('account')) {
+		if (!$request->request->get('account')) {
 			throw new ParameterMissingException("Parameter 'account' is missing");
 		}
 
@@ -106,7 +109,11 @@ class OauthController extends BaseController
 		$googleApi = $this->getGoogleApi();
 
 		try {
-			$client = new StorageApi($this->getRequest()->request->get('token'), null, $this->componentName);
+			$client = $sapi = new StorageApi([
+				'token' => $request->request->get('token'),
+				'url'   => null,
+				'userAgent' => $this->componentName
+			]);
 
 			$url = $googleApi->getAuthorizationUrl(
 				$this->container->get('router')->generate('keboola_google_drive_oauth_callback', array(), UrlGeneratorInterface::ABSOLUTE_URL),
@@ -115,8 +122,8 @@ class OauthController extends BaseController
 			);
 
 			$session->set('token', $client->getTokenString());
-			$session->set('account', $this->getRequest()->request->get('account'));
-			$session->set('referrer', $this->getRequest()->request->get('referrer'));
+			$session->set('account', $request->request->get('account'));
+			$session->set('referrer', $request->request->get('referrer'));
 
 			return new RedirectResponse($url);
 		} catch (\Exception $e) {
@@ -145,12 +152,17 @@ class OauthController extends BaseController
 		$googleApi = $this->getGoogleApi();
 
 		try {
-			$storageApi = new StorageApi($token, null, $this->componentName);
+			$storageApi = new StorageApi([
+				'token' => $token,
+				'url'   => null,
+				'userAgent' => $this->componentName
+			]);
 
 			/** @var EncryptorInterface $encryptor */
 			$encryptor = $this->get('syrup.encryptor');
 
-			$configuration = new Configuration($storageApi, $this->componentName, $encryptor);
+			$configuration = new Configuration($this->componentName, $encryptor);
+			$configuration->setStorageApi($storageApi);
 
 			$tokens = $googleApi->authorize($code, $this->container->get('router')->generate(
 				'keboola_google_drive_oauth_callback', array(), UrlGeneratorInterface::ABSOLUTE_URL)
